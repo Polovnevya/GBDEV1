9447513fbeaa7b8968deb8ddce36c814f8e79b90
Сергей <sergey_skrf@mail.ru>
2023-06-29 16:13:01 +0700

    Create sql_requests.py
    запросы к базе

---
+++ sql_requests.py
@@ -0,0 +1,17 @@
+# Количество откликов на вакансию
+my_request = (f'SELECT vacancies.name, COUNT(posts.id)\n'
+              f'FROM posts\n'
+              f'JOIN vacancies ON posts.vacancy_id = vacancies.id\n'
+              f'JOIN employers ON employers.id = vacancies.employer_id\n'
+              #f'WHERE employers.tg_id = {callback.from_user.id}\n'
+              #f'GROUP BY vacancies.id'
+              )
+
+# Количество опубликованных постов с вакансией--
+my_request1 = (f'SELECT vacancies.name, COUNT(posts.id)\n'
+               f'FROM posts\n'
+               f'JOIN vacancies ON posts.vacancy_id = vacancies.id\n'
+               f'JOIN employers ON employers.id = vacancies.employer_id\n'
+               #f'WHERE employers.tg_id = {callback.from_user.id}\n'
+               #f'GROUP BY vacancies.id'
+               )
