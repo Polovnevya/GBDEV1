452a5f87273250a60731fa63ebb7ed8f82e83df6
Сергей <sergey_skrf@mail.ru>
2023-06-30 10:16:28 +0700

    Update sql_requests.py
    request1 была ошибка (было request1=request2)

--- sql_requests.py
+++ sql_requests.py
@@ -1,17 +1,18 @@
 # Количество откликов на вакансию
-request1 = (f'SELECT vacancies.name, COUNT(posts.id)\n'
-              f'FROM posts\n'
-              f'JOIN vacancies ON posts.vacancy_id = vacancies.id\n'
-              f'JOIN employers ON employers.id = vacancies.employer_id\n'
-              #f'WHERE employers.tg_id = {callback.from_user.id}\n'
-              #f'GROUP BY vacancies.id'
-              )
+request1 = (f'vacancies.name, COUNT(candidates.id)\n'
+            f'FROM employers\n'
+            f'vacancies ON employers.id = vacancies.employer_id\n'
+            f'feedback ON vacancies.id = feedback.vacancy_id\n'
+            f'candidates ON feedback.candidate_id = candidates.id\n'
+            # f'WHERE employers.tg_id = {callback.from_user.id}\n'
+            # f'GROUP BY vacancies.id'
+            )
 
 # Количество опубликованных постов с вакансией--
 request2 = (f'SELECT vacancies.name, COUNT(posts.id)\n'
-               f'FROM posts\n'
-               f'JOIN vacancies ON posts.vacancy_id = vacancies.id\n'
-               f'JOIN employers ON employers.id = vacancies.employer_id\n'
-               #f'WHERE employers.tg_id = {callback.from_user.id}\n'
-               #f'GROUP BY vacancies.id'
-               )
+            f'FROM posts\n'
+            f'JOIN vacancies ON posts.vacancy_id = vacancies.id\n'
+            f'JOIN employers ON employers.id = vacancies.employer_id\n'
+            # f'WHERE employers.tg_id = {callback.from_user.id}\n'
+            # f'GROUP BY vacancies.id'
+            )
