6f6800e6b6f253c35c63ca9ab1641821c5c7ad86
Сергей <sergey_skrf@mail.ru>
2023-07-03 16:46:58 +0700

    Update personal_cabinet.py
    отредактировал запрос (отступы)

--- handlers/employer/personal_cabinet.py
+++ handlers/employer/personal_cabinet.py
@@ -84,25 +84,25 @@ async def download_document(message: Message, bot: Bot):
 async def process_button_2_press(callback: CallbackQuery,
                                  bot: Bot,):
     report = Reporting()
-    records = report.get_reporting(
-        f'WITH\n'
-    f'number_responses AS (\n'
-        f'SELECT vacancies.id, COUNT(candidates.id) AS count_responses\n'
-        f'FROM employers\n'
-        f'JOIN vacancies ON employers.id = vacancies.employer_id\n'
-        f'JOIN feedback ON vacancies.id = feedback.vacancy_id\n'
-        f'JOIN candidates ON feedback.candidate_id = candidates.id\n'
-        f'WHERE employers.tg_id = 3\n'
-        f'GROUP BY vacancies.id),\n'
-    f'number_posts AS (\n'
-        f'SELECT vacancies.id AS id, vacancies.name AS name, COUNT(posts.id) AS count_posts\n'
-        f'FROM posts\n'
-        f'JOIN vacancies ON posts.vacancy_id = vacancies.id\n'
-        f'JOIN employers ON employers.id = vacancies.employer_id\n'
-        f'WHERE employers.tg_id = {callback.from_user.id}\n'
-        f'GROUP BY vacancies.id)\n'
-    f'SELECT number_posts.id, number_posts.name, number_posts.count_posts, number_responses.count_responses\n'
-    f'FROM number_posts LEFT OUTER JOIN number_responses ON number_posts.id=number_responses.id;')
+    records = report.get_reporting(f'WITH\n'
+                                   f'number_responses AS (\n'
+                                        f'SELECT vacancies.id, COUNT(candidates.id) AS count_responses\n'
+                                        f'FROM employers\n'
+                                        f'JOIN vacancies ON employers.id = vacancies.employer_id\n'
+                                        f'JOIN feedback ON vacancies.id = feedback.vacancy_id\n'
+                                        f'JOIN candidates ON feedback.candidate_id = candidates.id\n'
+                                        f'WHERE employers.tg_id = 3\n'
+                                        f'GROUP BY vacancies.id),\n'
+                                    f'number_posts AS (\n'
+                                        f'SELECT vacancies.id AS id, vacancies.name AS name, COUNT(posts.id) AS count_posts\n'
+                                        f'FROM posts\n'
+                                        f'JOIN vacancies ON posts.vacancy_id = vacancies.id\n'
+                                        f'JOIN employers ON employers.id = vacancies.employer_id\n'
+                                        f'WHERE employers.tg_id = {callback.from_user.id}\n'
+                                        f'GROUP BY vacancies.id)\n'
+                                    f'SELECT number_posts.id, number_posts.name, number_posts.count_posts, number_responses.count_responses\n'
+                                    f'FROM number_posts LEFT OUTER JOIN number_responses ON number_posts.id=number_responses.id;'
+                                    )
     list_name_request = [('id',
                           'Наименование вакансии',
                           'Количество опубликованных постов с вакансией',
