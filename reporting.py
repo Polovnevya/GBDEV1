483362295ef6d36a6f24501d8d007fe7751af4f1
Сергей <sergey_skrf@mail.ru>
2023-07-04 12:37:27 +0700

    Update reporting.py
    конфиги прописал в __init__,
    скорректировал часть имён переменных

--- reporting.py
+++ reporting.py
@@ -4,17 +4,22 @@ from config.config import load_config
 
 class Reporting:
 
-    def __init__(self):
-        self.database = load_config().db.database
-        self.user = load_config().db.db_user
-        self.password = load_config().db.db_password
-        self.host = load_config().db.db_host
+    def __init__(self,
+                 database=load_config().db.database,
+                 db_user=load_config().db.db_user,
+                 db_password=load_config().db.db_password,
+                 db_host=load_config().db.db_host
+                 ):
+        self.database = database
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_host = db_host
 
     def get_cursor(self):
         con = psycopg2.connect(database=self.database,
-                               user=self.user,
-                               password=self.password,
-                               host=self.host)
+                               user=self.db_user,
+                               password=self.db_password,
+                               host=self.db_host)
         cursor = con.cursor()
         return cursor
 
